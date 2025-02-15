02d97d833d81ecec6dac1a7e112c9a102c87fe42
Yago <yaguitor@gmail.com>
2025-02-15 12:00:16 +0100

    Final Initial Config

---
+++ src/main/java/com/yago/ChatServer/ChatWebSocketHandler.java
@@ -0,0 +1,36 @@
+package com.yago.ChatServer;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private static final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        broadcastMessage(message.getPayload());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    private void broadcastMessage(String message) throws IOException {
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(message));
+            }
+        }
+    }
+}
