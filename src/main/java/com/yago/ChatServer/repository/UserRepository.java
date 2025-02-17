30ac6d2673951fe8d18abde532f0b831c1db5fd2
yago <yaguitor@gmail.com>
2025-02-17 19:46:39 +0100

    commit

--- src/main/java/com/yago/ChatServer/repository/UserRepository.java
+++ src/main/java/com/yago/ChatServer/repository/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
     User findByUsername(String username);
+
+    User findById(Long id);
+
     boolean existsByUsername(String username);
 }
